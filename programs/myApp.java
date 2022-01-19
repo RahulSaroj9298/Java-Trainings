21622f0ce8fc9d6ecb1cf6537519ed2623d0e93f
RahulSaroj9298 <rahulkumar.rk2651@gmail.com>
2022-01-19 14:13:00 +0530

    programs

---
+++ programs/myApp.java
@@ -0,0 +1,10 @@
+class myApp{
+    public static void main(String []args)
+    {
+        System.out.println("We are learning Java");
+        System.out.println("Be the Exceptional");
+    }
+}
+class One{
+    
+}
